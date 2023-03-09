7c3e011032d731c8a53712ebc5f870b195cb8c9b
Mpincai-valencia <127371288+Mpincai-valencia@users.noreply.github.com>
2023-03-09 00:00:49 -0500

    Create ResponseDTO.java

---
+++ src/main/java/co/edu/umanizales/manage_store/controller/dto/ResponseDTO.java
@@ -0,0 +1,15 @@
+package co.edu.umanizales.manage_store.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+public class ResponseDTO
+{
+    private int code;
+    private Object data;
+
+    private List<ErrorDTO>errors;
+}
