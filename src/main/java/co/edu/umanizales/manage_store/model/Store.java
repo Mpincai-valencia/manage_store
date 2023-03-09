c803b210ddd43a2ab7a8f2e54d557e6884a0ecd4
Mpincai-valencia <127371288+Mpincai-valencia@users.noreply.github.com>
2023-03-09 00:01:12 -0500

    Create Store.java

---
+++ src/main/java/co/edu/umanizales/manage_store/model/Store.java
@@ -0,0 +1,11 @@
+package co.edu.umanizales.manage_store.model;
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class Store {
+    private String code;
+    private String name;
+}
