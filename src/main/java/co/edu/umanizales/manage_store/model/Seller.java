9a8c7e472af9d33afb7f32155204a7ecba49a772
Mpincai-valencia <127371288+Mpincai-valencia@users.noreply.github.com>
2023-03-09 00:01:09 -0500

    Create Seller.java

---
+++ src/main/java/co/edu/umanizales/manage_store/model/Seller.java
@@ -0,0 +1,14 @@
+package co.edu.umanizales.manage_store.model;
+import lombok.*;
+import org.springframework.stereotype.Service;
+@AllArgsConstructor
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+public class Seller {
+    private String code;
+    private String name;
+
+
+}
